7b266141c5e21a5ab4f2bb94fa222cf8beccaa54
AidanHens <113548903+AidanHens@users.noreply.github.com>
2022-09-27 11:36:27 -0700

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -29,5 +29,7 @@ public class RenameThisClass
         String str2 = str1;
         String str3 = "I am also a string";
         Boolean stringsEqual = str1.equals(str2);
+        
+        String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
     }
 }
